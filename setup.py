c6ed773bff1b84ba657a5b2825d798e222142eab
Vincy.exe <vincysuper07@gmail.com>
2021-11-01 18:29:46 +0100

    I forgot I need to use different versions every time I publish on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ncspy",
-    version="0.1",
+    version="0.2",
     author="Vincy.zsh",
     author_email="Vincysuper07@gmail.com",
     description='A NoCopyrightSounds "API" wrapper written with asyncio.',
